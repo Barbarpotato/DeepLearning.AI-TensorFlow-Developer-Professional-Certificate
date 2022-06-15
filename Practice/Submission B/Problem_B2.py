c2efbfccb18c7eb28d08d8934543e3fb91dcb183
Darmawan <darmawanjr88@gmail.com>
2022-06-15 16:53:25 +0800

    added B1 and B2 Solution

---
+++ Practice/Submission B/Problem_B2.py
@@ -0,0 +1,50 @@
+# =============================================================================
+# PROBLEM B2
+#
+# Build a classifier for the Fashion MNIST dataset.
+# The test will expect it to classify 10 classes.
+# The input shape should be 28x28 monochrome. Do not resize the data.
+# Your input layer should accept (28, 28) as the input shape.
+#
+# Don't use lambda layers in your model.
+#
+# Desired accuracy AND validation_accuracy > 83%
+# =============================================================================
+
+import tensorflow as tf
+import numpy as np
+
+def solution_B2():
+    fashion_mnist = tf.keras.datasets.fashion_mnist
+
+    # YOUR CODE HERE
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+    x_train = x_train / 255.0
+    x_train = np.expand_dims(x_train, axis=-1)
+    x_test = np.expand_dims(x_test, axis=-1)
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+        tf.keras.layers.MaxPool2D(2, 2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=2, validation_data=(x_test, y_test))
+
+    return model
+
+
+# The code below is to save your model as a .h5 file.
+# It will be saved automatically in your Submission folder.
+if __name__ == '__main__':
+    model = solution_B2()
+    model.save("model_B2.h5")
+
+
